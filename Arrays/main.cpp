f3e24554dd9c999a8672e41d5bab60b2c4c899b3
Matt Wheatley <matthew.wheatley@snhu.edu>
2019-11-23 17:47:40 -0500

    Initial Commit

---
+++ Arrays/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Arrays
+//
+//  Created by Matt Wheatley on 11/23/19.
+//  Copyright © 2019 Matt Wheatley. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
